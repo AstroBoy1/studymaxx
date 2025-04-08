1d1a7bca7a5825a654af03df63817411c9493656
omorim <omorim@oregonstate.edu>
2025-04-07 18:01:31 -0700

    start port

--- server/start.sh
+++ server/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export FLASK_DEBUG=1
-export FLASK_ENV=development
+# export FLASK_DEBUG=1
+# export FLASK_ENV=development
 
-flask --app main run --port=5001
+# flask --app main run --port=5001
+export FLASK_ENV=production  # Set Flask to production mode
+
+flask --app main run --port=${PORT:-5000}  # Use the $PORT variable or default to 5000
